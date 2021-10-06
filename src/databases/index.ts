0f7bab4c91c1b57809f09c2a9669c7ce84283568
Alazar-dev <mealazartadesse@gmail.com>
2021-10-06 19:33:01 +0300

    updated database url

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -4,7 +4,8 @@ import { dbConfig } from '@interfaces/db.interface';
 const { host, port, database }: dbConfig = config.get('dbConfig');
 
 export const dbConnection = {
-  url: `mongodb://${host}:${port}/${database}`,
+  // url: `mongodb://${host}:${port}/${database}`,
+  url: 'mongodb://u2bmzx3w9p6qhy0z2blo:aM2yWTOdEgcZ35yjgPfZ@bpbzeqddsujbzu4-mongodb.services.clever-cloud.com:27017/bpbzeqddsujbzu4',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
